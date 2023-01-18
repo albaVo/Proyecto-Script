1bfe21b46f2ea99f98f91418a86d847f4526e826
albaVo <alba12vo@gmail.com>
2023-01-18 13:51:26 +0100

    Primera opción del menu

--- proyecto_script.sh
+++ proyecto_script.sh
@@ -1,1 +1,22 @@
 #!/bin/bash
+menu(){
+    echo "Elige una opción: "
+    echo "
+    1) Actualizar el software del sistema
+    ) Salir del menu"
+}
+
+actualizar(){
+    sudo apt update
+    sudo apt upgrade
+}
+
+while true;do
+    menu
+    read -p "Introduce una opción del menú anterior: " opcion
+    case $opcion in
+        1)actualizar;;
+        )exit;;
+        *)echo "Ha introducido una opción no válida";break;;
+    esac
+done
