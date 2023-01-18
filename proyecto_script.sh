84713a5dea35ac3e453de602f4eec530de054e08
albaVo <alba12vo@gmail.com>
2023-01-18 14:14:03 +0100

    Segunda opción del menu

--- proyecto_script.sh
+++ proyecto_script.sh
@@ -3,20 +3,36 @@ menu(){
     echo "Elige una opción: "
     echo "
     1) Actualizar el software del sistema
-    ) Salir del menu"
+    2) Instalar un paquete deseado
+    3) Salir del menu"
 }
 
 actualizar(){
     sudo apt update
     sudo apt upgrade
+    echo ""
+    echo "----- ACTUALIZACIÓN COMPLETADA -----"
+}
+
+instalar(){
+    echo "Busca el paquete que deseas instalar para ver si está disponible "
+    read -p "Introduce el nombre del paquete: " paquete
+    apt-cache search $paquete
+    echo ""
+    read -p "Elige una de las opciones para instalar " opcion
+    sudo apt-get install $opcion
+    echo ""
+    echo "----- PAQUETE INSTALADO -----"
+    echo ""
 }
 
 while true;do
     menu
     read -p "Introduce una opción del menú anterior: " opcion
     case $opcion in
-        1)actualizar;;
-        )exit;;
+        1)clear;actualizar;;
+        2)clear;instalar;;
+        3)exit;;
         *)echo "Ha introducido una opción no válida";break;;
     esac
 done
